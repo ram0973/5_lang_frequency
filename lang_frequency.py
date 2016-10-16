066f02627e4cffb79b7882660bd42d208458ebe0
ram0973 <ram0973@gmail.com>
2016-10-16 16:54:20 +0300

    fixes

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,45 +1,67 @@
 # -*- coding: utf-8 -*-
 import argparse
-from collections import Counter
-import os
 import re
 import sys
+from collections import Counter
+from colorama import Fore, Style
+
+MOST_FREQUENT_WORDS_COUNT_DEFAULTS = 10
 
 
 def load_win_unicode_console():
+    """
+    Включаем правильное отображение unicode в консоли под MS Windows
+    и раскрашивание символов
+    """
     if sys.platform == 'win32':
         import win_unicode_console
         win_unicode_console.enable()
+        from colorama import init
+        init()  # colorama
 
 
-def get_named_argument(arg_name: str) -> str:
-    if len(sys.argv) > 1:
-        parser = argparse.ArgumentParser()
-        parser.add_argument('--' + arg_name)
-        return getattr(parser.parse_args(sys.argv[1:]), arg_name)
+def get_text_file_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--text', help='Введите путь к текстовому файлу')
+    parser.add_argument('--count',
+                        help='Введите количество слов',
+                        default=MOST_FREQUENT_WORDS_COUNT_DEFAULTS,
+                        type=int)
+    if parser.parse_args().text:
+        return parser.parse_args().text, parser.parse_args().count
     else:
-        print('Введите параметр в формате --%s Значение' % arg_name)
+        parser.print_help()
         exit(1)
 
 
-def load_data(filepath: str):
-    if os.path.isfile(filepath):
-        try:
-            with open(filepath, mode='r') as file:
-                return re.findall(r'[^\W|\d]+', file.read().lower())
-        except PermissionError:
-            print('У вас нет прав доступа к файлу')
-            exit(1)
-    else:
-        print('Файл не найден')
-        exit(1)
+def load_data(file_path: str):
+    with open(file_path, mode='r') as file:
+        return re.findall(r'[^\W|\d]+', file.read().lower())
 
 
-def get_most_frequent_words(text):
-    return Counter(text).most_common(10)
+def get_most_frequent_words(text, count):
+    return Counter(text).most_common(count)
+
+
+def print_most_frequent_words_list(words_list):
+    for number in range(len(words_list)):
+        print('%d. Слово: "%s" Частота использования: %s раз' % (number + 1,
+              words_list[number][0], words_list[number][1]))
 
 
 if __name__ == '__main__':
-    file_path = get_named_argument('file')
+
+    text_file_path, top_words_count = get_text_file_arguments()
+
+    try:
+        text_for_analyze = load_data(text_file_path)
+    except OSError as error:
+        print(Fore.RED+Style.BRIGHT, 'Ошибка: ', error.strerror, ' в файле: ',
+              error.filename)
+        exit(1)
+
     load_win_unicode_console()
-    print(get_most_frequent_words(load_data(file_path)))
+
+    most_frequent_words = get_most_frequent_words(
+        text_for_analyze, top_words_count)
+    print_most_frequent_words_list(most_frequent_words)
